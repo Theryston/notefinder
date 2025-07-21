480006328c2a7681b6198741e20dc15967185336
Theryston <devtheryston@gmail.com>
2025-07-21 13:29:01 -0300

    feat: process

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.responses import JSONResponse
 import uvicorn
 from pipeline import pipeline
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.post("/pipeline")
 async def run_pipeline(request: Request):
@@ -16,6 +21,25 @@ async def run_pipeline(request: Request):
     
     notes = pipeline(content_path, content_type)
     return JSONResponse(status_code=200, content={"notes": notes})
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+    return {"path": file_location}
+
+@app.get("/")
+async def web_file():
+    return FileResponse("web/index.html")
+
+@app.get("/{filepath:path}")
+async def serve_static_files(filepath: str):
+    file_path = os.path.join("web", filepath)
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        return FileResponse(file_path)
+    return JSONResponse(status_code=404, content={"error": "File not found"})
+
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
