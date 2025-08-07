cc66513a854d4af5c8079fcc4996dd9d68093c6f
Theryston <devtheryston@gmail.com>
2025-08-07 18:49:31 -0300

    fixes

--- frontend/src/components/HomeClient.tsx
+++ frontend/src/components/HomeClient.tsx
@@ -44,8 +44,10 @@ export default function HomeClient() {
           </Link>
         </div>
         {predictions.length === 0 ? (
-          <div className="text-center py-12 border rounded-2xl">
-            <p className="text-gray-600">Nenhuma predição encontrada</p>
+          <div className="text-center py-12 border border-gray-200 p-5 bg-white dark:border-none dark:bg-gray-700 rounded-2xl">
+            <p className="text-gray-600 dark:text-gray-400">
+              Nenhuma predição encontrada
+            </p>
             <Link
               href="/new"
               className="mt-4 inline-block px-4 py-2 rounded-full bg-brand-600 text-white"
