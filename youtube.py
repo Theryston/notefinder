918fd726ae7ae9f756306c37f38c687575dfb13c
Theryston <devtheryston@gmail.com>
2025-08-08 19:44:18 -0300

    fix: yt

--- youtube.py
+++ youtube.py
@@ -3,14 +3,9 @@ import os
 
 
 def download_youtube_audio(url: str, output_path: str):
-    cookies_path = os.path.join(os.path.dirname(__file__), 'cookies.txt')
-    if not os.path.exists(cookies_path):
-        raise FileNotFoundError(f"Cookies file not found at {cookies_path}")
-    
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': output_path,
-        'cookiefile': cookies_path,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'wav',
