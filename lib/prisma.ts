2d02d1bedc9708079647a88b613a6d2b589e9971
Theryston Santos <devtheryston@gmail.com>
2025-10-07 17:31:26 -0300

    fix: prisma type

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
+function createPrismaClient() {
+  return new PrismaClient({
+    log:
+      process.env.NODE_ENV !== 'production'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  }).$extends(withAccelerate());
+}
+
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+  prisma: ReturnType<typeof createPrismaClient>;
 };
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  }).$extends(withAccelerate());
+const prisma = globalForPrisma.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
