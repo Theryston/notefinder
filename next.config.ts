b6c18a04a7035c5f4de280a6a9b2c73b6c830002
Theryston Santos <devtheryston@gmail.com>
2025-10-29 11:20:11 -0300

    fix: image hostname for s3 bucket

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'notefinder-files.s3.us-east-1.amazonaws.com',
+      },
     ],
   },
 };
