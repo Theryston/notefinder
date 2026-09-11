dfa4b50147c0ba7576f92db447f6914a77a51da7
Theryston Santos <devtheryston@gmail.com>
2026-09-11 14:46:23 -0300

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,20 @@
-export { auth as proxy } from './auth';
+import { auth } from './auth';
+
+export const proxy = auth((request) => {
+  console.log(
+    JSON.stringify({
+      type: 'incoming-request',
+      method: request.method,
+      pathname: request.nextUrl.pathname,
+      search: request.nextUrl.search,
+      ip:
+        request.headers.get('fly-client-ip') ??
+        request.headers.get('x-forwarded-for'),
+      userAgent: request.headers.get('user-agent'),
+      timestamp: new Date().toISOString(),
+    }),
+  );
+});
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
